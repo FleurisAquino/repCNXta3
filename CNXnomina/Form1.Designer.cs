579fe44dbfc2d9769c6e4dbcc693c2d1037188be
Juan Aquino <fleuris1505@hotmail.com>
2017-11-19 01:55:24 -0400

    Arreglos

--- CNXnomina/Form1.Designer.cs
+++ CNXnomina/Form1.Designer.cs
@@ -28,12 +28,356 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btcrear = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel_datos_emp = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.cbsexo = new System.Windows.Forms.ComboBox();
+            this.tbapellido = new System.Windows.Forms.TextBox();
+            this.tbfechai = new System.Windows.Forms.TextBox();
+            this.tbfechan = new System.Windows.Forms.TextBox();
+            this.tbnombre = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel_datos_emp.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(287, 482);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 18;
+            this.button6.Text = "Borrar ";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(402, 482);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "Imprimir ";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(178, 482);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Guardar";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // btcrear
+            // 
+            this.btcrear.Location = new System.Drawing.Point(66, 482);
+            this.btcrear.Name = "btcrear";
+            this.btcrear.Size = new System.Drawing.Size(75, 23);
+            this.btcrear.TabIndex = 15;
+            this.btcrear.Text = "Crear";
+            this.btcrear.UseVisualStyleBackColor = true;
+            this.btcrear.Click += new System.EventHandler(this.btcrear_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(15, 343);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(50, 13);
+            this.label11.TabIndex = 14;
+            this.label11.Text = "Calculos ";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.textBox8);
+            this.panel2.Controls.Add(this.textBox7);
+            this.panel2.Location = new System.Drawing.Point(10, 343);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(512, 108);
+            this.panel2.TabIndex = 13;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(309, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 26);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Calcular Antiguedad";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(309, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Calcular Edad";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(132, 52);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(153, 20);
+            this.textBox8.TabIndex = 1;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(132, 16);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(153, 20);
+            this.textBox7.TabIndex = 0;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(18, 228);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(39, 13);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Salario";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.textBox6);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Location = new System.Drawing.Point(10, 228);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(512, 96);
+            this.panel1.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(132, 37);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(148, 20);
+            this.textBox6.TabIndex = 2;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(74, 40);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(39, 13);
+            this.label10.TabIndex = 1;
+            this.label10.Text = "Salario";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Datos Generales ";
+            // 
+            // panel_datos_emp
+            // 
+            this.panel_datos_emp.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel_datos_emp.Controls.Add(this.pictureBox1);
+            this.panel_datos_emp.Controls.Add(this.label8);
+            this.panel_datos_emp.Controls.Add(this.cbsexo);
+            this.panel_datos_emp.Controls.Add(this.tbapellido);
+            this.panel_datos_emp.Controls.Add(this.tbfechai);
+            this.panel_datos_emp.Controls.Add(this.tbfechan);
+            this.panel_datos_emp.Controls.Add(this.tbnombre);
+            this.panel_datos_emp.Controls.Add(this.label7);
+            this.panel_datos_emp.Controls.Add(this.label6);
+            this.panel_datos_emp.Controls.Add(this.label4);
+            this.panel_datos_emp.Controls.Add(this.label3);
+            this.panel_datos_emp.Controls.Add(this.label2);
+            this.panel_datos_emp.Location = new System.Drawing.Point(10, 35);
+            this.panel_datos_emp.Name = "panel_datos_emp";
+            this.panel_datos_emp.Size = new System.Drawing.Size(512, 164);
+            this.panel_datos_emp.TabIndex = 10;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(367, 44);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 110);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(364, 21);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(57, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Fotografia:";
+            // 
+            // cbsexo
+            // 
+            this.cbsexo.FormattingEnabled = true;
+            this.cbsexo.Items.AddRange(new object[] {
+            "M",
+            "H"});
+            this.cbsexo.Location = new System.Drawing.Point(132, 67);
+            this.cbsexo.Name = "cbsexo";
+            this.cbsexo.Size = new System.Drawing.Size(121, 21);
+            this.cbsexo.TabIndex = 2;
+            // 
+            // tbapellido
+            // 
+            this.tbapellido.Location = new System.Drawing.Point(132, 44);
+            this.tbapellido.Name = "tbapellido";
+            this.tbapellido.Size = new System.Drawing.Size(197, 20);
+            this.tbapellido.TabIndex = 3;
+            // 
+            // tbfechai
+            // 
+            this.tbfechai.Location = new System.Drawing.Point(132, 115);
+            this.tbfechai.Name = "tbfechai";
+            this.tbfechai.Size = new System.Drawing.Size(197, 20);
+            this.tbfechai.TabIndex = 8;
+            // 
+            // tbfechan
+            // 
+            this.tbfechan.Location = new System.Drawing.Point(132, 91);
+            this.tbfechan.Name = "tbfechan";
+            this.tbfechan.Size = new System.Drawing.Size(197, 20);
+            this.tbfechan.TabIndex = 7;
+            // 
+            // tbnombre
+            // 
+            this.tbnombre.Location = new System.Drawing.Point(132, 21);
+            this.tbnombre.Name = "tbnombre";
+            this.tbnombre.Size = new System.Drawing.Size(197, 20);
+            this.tbnombre.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(23, 118);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(90, 13);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Fecha de Ingreso";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(23, 94);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Fecha de Nacimiento";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Sexo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Apellido";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nombre ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 574);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btcrear);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel_datos_emp);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel_datos_emp.ResumeLayout(false);
+            this.panel_datos_emp.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btcrear;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel_datos_emp;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox cbsexo;
+        private System.Windows.Forms.TextBox tbapellido;
+        private System.Windows.Forms.TextBox tbfechai;
+        private System.Windows.Forms.TextBox tbfechan;
+        private System.Windows.Forms.TextBox tbnombre;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- CNXnomina/Form1.cs
+++ CNXnomina/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace CNXnomina
 {
@@ -15,6 +16,19 @@ namespace CNXnomina
         public Form1()
         {
             InitializeComponent();
+            pictureBox1.Image = Image.FromFile("user.png");// imagen de usuario se guardo en la ruta del bin del proyecto
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btcrear_Click(object sender, EventArgs e)
+        {
+            Operaciones oper = new Operaciones();
+            oper.ConsultaSinResultado(" INSERT INTO empleado (nombre, apellido,fecha_nacimiento,fcha_ingreso,sexo)VALUES('" + tbnombre.Text.ToString() + "','" + tbapellido.Text.ToString() + "','" + tbfechan.Text.ToString() + "','" + tbfechai.Text.ToString() + "','" + cbsexo.Text.ToString() + "')");
+
         }
     }
 }
